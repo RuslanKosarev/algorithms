f3e38f89fa8758d490ed6246779c874573fa95c6
korus <ruslan.kosarev@smedx.com>
2021-09-03 09:31:57 +0300

    fill the matrix diagonally

---
+++ matrix.py
@@ -0,0 +1,27 @@
+"""
+fill the matrix diagonally
+"""
+
+import numpy as np
+
+N = 5
+a = np.zeros([N, N])
+
+count = 0
+
+for d in range(N):
+    for k in range(d+1):
+        count += 1
+        a[k, d-k] = count
+        print(a)
+
+for d in range(1, N):
+    for k in range(N-d):
+        count += 1
+        a[d+k, N-1-k] = count
+        print(a)
+
+print(a)
+
+
+
